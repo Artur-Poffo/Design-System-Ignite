0c45b40ce360f9f2964879b59bdcb25f39897b09
Artur Poffo <arturpoffoc@hotmail.com>
2023-04-08 01:11:39 -0300

    ci: Add worflow to deploy docs

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -6,10 +6,14 @@ const config: StorybookConfig = {
   stories: ['../src/pages/**/*.mdx', '../src/stories/**/*.stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: '@storybook/react-vite',
-  async viteFinal(config, options) {
-    // Add your configuration here
+  async viteFinal(config, { configType }) {
+    if (configType === 'PRODUCTION') {
+      config.base = '/Design-System-Ignite/'
+    }
+    
     return config;
   },
+  
 };
 
 export default config;
