010a6fadfc3f0dbe74feb97e4f25d6967fd11a57
Artur Poffo <arturpoffoc@hotmail.com>
2023-04-08 21:16:08 -0300

    fix: Trying again

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -6,14 +6,16 @@ const config: StorybookConfig = {
   stories: ['../src/pages/**/*.mdx', '../src/stories/**/*.stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: '@storybook/react-vite',
-  async viteFinal(config, { configType }) {
+  core: {
+    builder: '@storybook/builder-vite'
+  },
+  viteFinal(config, { configType }) {
     if (configType === 'PRODUCTION') {
       config.base = '/Design-System-Ignite/'
     }
-    
+  
     return config;
   },
-  
 };
 
 export default config;
