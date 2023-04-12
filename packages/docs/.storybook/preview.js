7d268b8a328fabd1bf0778a97ada599e17d24827
Artur Poffo <arturpoffoc@hotmail.com>
2023-04-12 10:15:21 -0300

    taking tests

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,19 +1,14 @@
-import { themes } from "@storybook/theming";
+import { themes } from '@storybook/theming'
 
-/** @type { import('@storybook/react').Preview } */
-const preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
-    docs: {
-      theme: themes.dark
-    }
   },
-};
-
-export default preview;
+  docs: {
+    theme: themes.dark,
+  }
+}
